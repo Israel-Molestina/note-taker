fa20aa141ec5cf9c4b33237aa04152d294c38da4
Israel Molestina <molestinaisrael@gmail.com>
2021-03-10 18:22:17 -0500

    added express.static to express.js file so css and index file are linked correnctly

--- express.js
+++ express.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 // this sets up express for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // point server to route files
 require('./routes/apiRoutes')(app);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,26 @@
-const noteData = require('../db/db.json');
-
+const notes = require('../db/db.json');
+const uniqid = require('uniqid');
+const fs = require('fs');
 module.exports = (app) => {
 
     // get request to db.json file
-    app.get('/api/notes', (req, res) => res.json(noteData));
+    app.get('/api/notes', (req, res) => res.json(notes));
+    // fs.readFile(path.join(__dirname, "../db/db.json"), 'utf-8', (err, notes) => {
+    //     if (err) throw err;
+    //     res.json(JSON.parse(notes))
+    // })
 
     // post request to db.json file
     app.post('/api/notes', (req, res) => {
-        noteData.push(req.body)
-        res.json(req.body);
+        const note = req.body
+        let newNote = {
+            "title": note.title,
+            "text": note.text,
+            "id": uniqid()
+        }
+        notes.push(newNote)
+        res.json(note);
+        console.log(note)
     });
+
 }
